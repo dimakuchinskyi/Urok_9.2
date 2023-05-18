c8fe65fe64e2eb5f45735457b8c1c202a3492e41
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-18 19:45:31 +0300

    Завдання 1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+'''
 #Ітератор
 lst = [1,2,3,4,5,6,6]
 print(iter(lst))
@@ -23,3 +23,42 @@ def my_generator(data):
 
 for num in my_generator(lst):
     print(num)
+
+def calc():
+    def add(a, b):
+        return a + b
+    def sub(a, b):
+        return a - b
+    def mult(a, b):
+        return a * b
+    def div(a, b):
+        if b != 0:
+            return a / b
+        else:
+            raise ValueError('Помилка! Ділення на нуль!')
+    return add, sub, mult, div
+add, sub, mult, div = calc()#Відбувається замикання
+print(div(3, 1))
+'''
+#Лабороторна робота
+#1
+class PrimeGenerator:
+    def __init__(self):
+        self.last_prime = 1
+
+    def is_prime(self, num):
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    def generate(self):
+        while True:
+            self.last_prime += 1
+            try:
+                if self.is_prime(self.last_prime):
+                    yield self.last_prime
+            except Exception as e:
+                print(f"Виникла помилка: {str(e)}")
