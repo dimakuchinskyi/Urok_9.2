539c3950decac1ec0816a26a7a531ce21e111962
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-18 19:09:44 +0300

    Концепція ітератора

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #Ітератор
 lst = [1,2,3,4,5,6,6]
 print(iter(lst))
+class MyIterator:
+    def __init__(self, data):
+        self.date = data
+        self.index = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        value = self.data[self.index]
+        self.index += 1
+        return value
+for num in MyIterator(lst):
+    print(num)
